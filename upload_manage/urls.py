0ca42261c22994612b8853c72f8bc75046920b4e
Harshil Suthar <sharshil07@gmail.com>
2020-11-20 16:42:27 +0530

    add delete in history and add some css files

--- upload_manage/urls.py
+++ upload_manage/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'upload_manage'
 urlpatterns = [
-    path('',views.history, name='History')
+    path('',views.history, name='History'),
+    path('delete_history/<int:pk>',views.DeleteHistory.as_view(), name='DeleteHistory')
 ]
--- upload_manage/views.py
+++ upload_manage/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from myapp.models import CsvErrorFile
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 @login_required
 def history(request):
@@ -9,3 +12,10 @@ def history(request):
     # user = request.user
     error_model = CsvErrorFile.objects.filter(user=user1).order_by('-upload_time')
     return render(request, 'history.html', {'history_data':error_model})
+
+
+class DeleteHistory(LoginRequiredMixin,DeleteView):
+    model = CsvErrorFile
+    success_url = reverse_lazy('upload_manage:History')
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
